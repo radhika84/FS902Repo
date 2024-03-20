32d70bbf55d692705c6134eda5e55cddce377ffa
radhi <radhika.rajesh06@gmail.com>
2024-03-20 14:59:19 -0400

    DEv1-Branch1

--- DemoProj1/src/com/nt/payment/PaymentOperationService.java
+++ DemoProj1/src/com/nt/payment/PaymentOperationService.java
@@ -1,5 +1,12 @@
 package com.nt.payment;
 
 public class PaymentOperationService {
+	
+	public String doCreditCarfdPayment() {
+		return "Credit card paymnet is done";
+	}
 
+	public String doDebitCardPayment() {
+		return "Debit card payment is done";
+	}
 }
